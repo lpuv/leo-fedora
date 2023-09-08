ca438fd439b95742485b5743dd1d04bc8808d83e
Leo Puvilland <leo@craftcat.dev>
2023-09-08 10:36:42 -0700

    another way to install rpmfusion

--- scripts/build.sh
+++ scripts/build.sh
@@ -52,7 +52,14 @@ run_scripts() {
 run_scripts "pre"
 
 
- 
+# RPMFusion
+rpm-ostree install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+rpm-ostree install rpmfusion-free-release rpmfusion-nonfree-release \
+    --uninstall rpmfusion-free-release \
+    --uninstall rpmfusion-nonfree-release
+
 
 # Install RPMs.
 get_yaml_array install_rpms '.rpm.install[]'
