f4c3780f87ac4882d7abe4731742bd1ede49bbf0
Leo Puvilland <leo@craftcat.dev>
2024-01-10 13:22:55 -0800

    one more try

--- config/scripts/power-profiles.sh
+++ config/scripts/power-profiles.sh
@@ -6,6 +6,6 @@ set -oue pipefail
 
 wget https://copr.fedorainfracloud.org/coprs/mariolimonciello/power-profiles-daemon/repo/fedora-39/mariolimonciello-power-profiles-daemon-39.repo -O /etc/yum.repos.d/mariolimonciello-power-profiles-daemon-39.repo
 sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates.repo
-rpm-ostree install power-profiles-daemon
+rpm-ostree install power-profiles-daemon --allow-inactive
 sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates.repo
 
