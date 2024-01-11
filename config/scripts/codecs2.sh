717e3d6aa6ecb04a806035c8494048c2e0ff1e02
Leo Puvilland <leo@craftcat.dev>
2024-01-10 21:56:26 -0800

    run script twice? pt 2

--- config/scripts/codecs2.sh
+++ config/scripts/codecs2.sh
@@ -4,6 +4,10 @@
 set -oue pipefail
 
 #rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+<<<<<<< HEAD
 rpm-ostree override remove mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld
+=======
+rpm-ostree override uninstall mesa-va-drivers
+>>>>>>> baa88bb (run script twice? pt 2)
 rpm-ostree install mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld --allow-inactive
 
