faf9ac38ed844524052d84d9738431d0517717d4
Leo Puvilland <leo@craftcat.dev>
2024-01-10 21:48:19 -0800

    some debugging

--- config/scripts/codecs.sh
+++ config/scripts/codecs.sh
@@ -4,6 +4,8 @@
 set -oue pipefail
 
 rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+rpm -q mesa-va-drivers
+rpm -q mesa-va-drivers-freeworld
 rpm-ostree override remove mesa-va-drivers*
 rpm-ostree install mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld --allow-inactive
 
