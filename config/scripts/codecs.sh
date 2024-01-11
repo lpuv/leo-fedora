06d7562605da6c6a014b456399d74cf622455dec
Leo Puvilland <leo@craftcat.dev>
2024-01-10 21:33:30 -0800

    pesky multimedia repo pt 2

--- config/scripts/codecs.sh
+++ config/scripts/codecs.sh
@@ -3,7 +3,7 @@
 
 set -oue pipefail
 
-sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-multimedia.repo
+rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 #rpm-ostree override remove mesa-va-drivers
 rpm-ostree install mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld --allow-inactive
-sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-multimedia.repo
+
