8a352a839cb54080ce8c04f6cd223a3188217940
Leo Puvilland <leo@craftcat.dev>
2024-01-10 22:01:15 -0800

    run script twice? pt 3

--- config/scripts/codecs.sh
+++ config/scripts/codecs.sh
@@ -4,7 +4,7 @@
 set -oue pipefail
 
 rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-rpm -q mesa-va-drivers
-rpm -q mesa-va-drivers-freeworld
+#rpm -q mesa-va-drivers
+#rpm -q mesa-va-drivers-freeworld
 rpm-ostree install mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld --allow-inactive
 
