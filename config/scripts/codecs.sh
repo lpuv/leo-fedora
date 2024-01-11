ff548de0a2c280ac691179bd7589fa99ceff5c16
Leo Puvilland <leo@craftcat.dev>
2024-01-10 21:42:21 -0800

    not sure which codecs are here

--- config/scripts/codecs.sh
+++ config/scripts/codecs.sh
@@ -7,9 +7,15 @@ set -oue pipefail
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates.repo
 sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates-archive.repo
 sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora.repo
+=======
+rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+rpm-ostree override remove mesa-va-drivers*
+rpm-ostree install mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld --allow-inactive
+>>>>>>> 1f9b07c (not sure which codecs are here)
 
 rpm-ostree override remove mesa-va-drivers-freeworld
 rpm-ostree install mesa-va-drivers
