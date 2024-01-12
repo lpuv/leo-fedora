e45d8244b26f2dbeca0262828e105e13ffb06a34
Leo Puvilland <leo@craftcat.dev>
2024-01-11 18:34:33 -0800

    codec script AGAIN

--- config/scripts/codecs.sh
+++ config/scripts/codecs.sh
@@ -3,9 +3,7 @@
 
 set -oue pipefail
 
+sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates.repo
+rpm-ostree install mesa-va-drivers
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates.repo
 rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-rpm-ostree override remove mesa-filesystem mesa-dri-drivers mesa-va-drivers-freeworld libva
-#rpm -q mesa-va-drivers
-#rpm -q mesa-va-drivers-freeworld
-rpm-ostree install libva mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld mesa-filesystem mesa-dri-drivers
-
