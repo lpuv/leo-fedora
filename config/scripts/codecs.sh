935287acb5fc3dcee8365a580d90907ad130e14c
Leo Puvilland <leo@craftcat.dev>
2024-01-11 18:42:31 -0800

    codec script AGAIN pt 2

---
+++ config/scripts/codecs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+
+set -oue pipefail
+
+sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates.repo
+sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates-archive.repo
+rpm-ostree install mesa-va-drivers
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates.repo
+sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates-archive.repo
+rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
