60ae457c76fb69f366e2409cf7250096026614e7
Leo Puvilland <leo@craftcat.dev>
2024-01-11 18:07:51 -0800

    retry codecs script pt 3

--- config/scripts/codecs.sh
+++ config/scripts/codecs.sh
@@ -4,8 +4,8 @@
 set -oue pipefail
 
 rm /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-rpm-ostree override remove mesa-filesystem
+rpm-ostree override remove mesa-filesystem mesa-dri-drivers mesa-va-drivers-freeworld
 #rpm -q mesa-va-drivers
 #rpm -q mesa-va-drivers-freeworld
-rpm-ostree install mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld mesa-filesystem
+rpm-ostree install mesa-va-drivers-freeworld mesa-vdpau-drivers-freeworld mesa-filesystem mesa-dri-drivers
 
