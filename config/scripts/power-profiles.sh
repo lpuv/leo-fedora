c2b0cc42c57a6ab0a98e4ed43e2158a876080ee2
Leo Puvilland <leo@craftcat.dev>
2024-01-16 09:36:13 -0800

    leftover merging stuff

--- config/scripts/power-profiles.sh
+++ config/scripts/power-profiles.sh
@@ -6,16 +6,7 @@ set -oue pipefail
 
 wget https://copr.fedorainfracloud.org/coprs/mariolimonciello/power-profiles-daemon/repo/fedora-39/mariolimonciello-power-profiles-daemon-39.repo -O /etc/yum.repos.d/mariolimonciello-power-profiles-daemon-39.repo
 sed -i '0,/enabled=1/s//enabled=0/' /etc/yum.repos.d/fedora-updates.repo
-<<<<<<< HEAD
-<<<<<<< HEAD
 rpm-ostree override remove power-profiles-daemon
 rpm-ostree install power-profiles-daemon-0.13-6 --allow-inactive
-=======
-rpm-ostree install power-profiles-daemon --allow-inactive
->>>>>>> f4c3780 (one more try)
-=======
-rpm-ostree override remove power-profiles-daemon
-rpm-ostree install power-profiles-daemon-0.13-6 --allow-inactive
->>>>>>> 3173896 (remove them add)
 sed -i '0,/enabled=0/s//enabled=1/' /etc/yum.repos.d/fedora-updates.repo
 rm /etc/yum.repos.d/mariolimonciello-power-profiles-daemon-39.repo
